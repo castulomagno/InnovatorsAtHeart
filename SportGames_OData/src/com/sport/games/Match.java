0e3e8805aea6453912c18cfdc9b65e07924984e3
raul-emilio-caraballo <raul-emilio.caraballo@hpe.com>
2017-06-30 16:57:03 -0500

    Updating Entities imports

--- SportGames_OData/src/com/sport/games/Match.java
+++ SportGames_OData/src/com/sport/games/Match.java
@@ -1,9 +1,18 @@
 package com.sport.games;
 
 import java.io.Serializable;
-import javax.persistence.*;
 import java.sql.Time;
 import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 
 /**
